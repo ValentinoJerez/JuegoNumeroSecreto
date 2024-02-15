d615b0c9353b9c2e0bae5e0f900b54d41b276daa
ValentinoJerez <valenjerez69@gmail.com>
2024-02-15 17:51:46 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML =  'Juego del Numero Secreto';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = "Indica un numero entre 1 y 10";
